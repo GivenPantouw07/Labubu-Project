fcab57908f6edb7b520dfa3bd44bbfcec1a6b06d
Voo-18 <s2221066@student.unklab.ac.id>
2024-10-29 18:36:26 +0800

    adding Generations, Gen1, Gen2

---
+++ labubu-react/src/components/Header/Generations/index.js
@@ -0,0 +1,16 @@
+import Gen1 from "./Gen1";
+import Gen2 from "./Gen2";
+
+const Generations = () => {
+  return (
+    <>
+      <h3>Generations</h3>;
+      <p>
+      <Gen1 />
+      <Gen2 /> 
+      </p>
+    </>
+  );
+};
+
+export default Generations;
--- labubu-react/src/components/Header/index.js
+++ labubu-react/src/components/Header/index.js
@@ -1,7 +1,14 @@
+import Generations from "./Generations";
+
 const header = () => {
     return (
+      <>
         <h1>Header</h1>
-    )
+        <p>
+            <Generations />
+        </p>
+      </>
+    );
 }
 
 export default header;
