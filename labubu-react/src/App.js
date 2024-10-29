423e4b21afd4a932198e1925f5c2b786ebe0ff5a
GivenPantouw07 <givenazarya@gmail.com>
2024-10-29 17:50:54 +0800

    adding Header

--- labubu-react/src/App.js
+++ labubu-react/src/App.js
@@ -1,28 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <h1>Hello Given</h1>
-          <h1>Hello Elsha</h1>
-          <h1>Hello guyss</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
-}
+};
 
 export default App;
