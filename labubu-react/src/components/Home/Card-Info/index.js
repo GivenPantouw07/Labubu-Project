728351773081d131a60d503f77944bc29211413b
Mayjennnn <Rarentewanmayjen@gmail.com>
2024-10-29 18:13:55 +0800

    adding Evolution, Type-Effect, Logo

--- labubu-react/src/components/Home/Card-Info/index.js
+++ labubu-react/src/components/Home/Card-Info/index.js
@@ -1,6 +1,14 @@
+import Evolution from './Evolution'
+import TE from './Type-Effect'
+import Logo from './Logo'
+
+
 const ci = () => {
     return (
+        <>
         <h2>Card-Info</h2>
+        <p><Evolution /><TE /><Logo /></p>
+        </>
     )
 }
 
