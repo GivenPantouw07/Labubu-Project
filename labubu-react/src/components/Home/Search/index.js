7c7e246e0e4c64e9f261f7b699e540e6e8e68804
elshaddai tambuwun <elshatambuwun17@gmail.com>
2024-10-30 16:14:29 +0700

    adding props in Search and Card-Info

--- labubu-react/src/components/Home/Search/index.js
+++
@@ -1,7 +0,0 @@
-const search = () => {
-    return (
-        <h2>Search</h2>
-    )
-}
-
-export default search;
---
+++ src/components/Home/Search/index.js
@@ -0,0 +1,10 @@
+const search = ({text,number}) => {
+    return (
+        <>
+        <h2>Search</h2>
+        <h5>{`${text} ${number}`}</h5>
+        </>
+    )
+}
+
+export default search;
