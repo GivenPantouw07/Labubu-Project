4007276b01c78416be2348b4adb9837750efbb70
stenlyadam <stenly.adam@unklab.ac.id>
2024-10-30 09:28:51 +0800

    update props

--- labubu-react/src/App.js
+++ labubu-react/src/App.js
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-      <Header text="header" />
+      <Header text="header using pull request" />
       <Home />
       <Footer />
     </>
