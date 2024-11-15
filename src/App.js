24196fe838eb184823dedb2215e5f7c7a1a06c84
Voo-18 <s2221066@student.unklab.ac.id>
2024-11-15 17:54:54 +0800

    adding Service

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Topbar from "./components/About/Topbar";
+import Topbar from "./components/Topbar";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
+import Service from "./components/Service";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Topbar />
       <Navbar />
       <About />
+      <Service />
     </div>
   );
 };
