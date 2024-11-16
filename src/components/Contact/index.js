875ba770c187be074b269654bf5b28d3a47594e8
elshaddai tambuwun <elshatambuwun17@gmail.com>
2024-11-17 00:59:52 +0700

    fixed contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,13 +3,31 @@ import React from "react";
 function Contact() {
   return (
     <div className="contact">
+        {/* header page contact start */}
+        <div className="page-header">
+            <div className="container">
+                <div className="row">
+                <div className="col-12">
+                    <h2>Contact Us</h2>
+                </div>
+                <div className="col-12">
+                    <a href="/">Home</a>
+                    <a href="/contact">Contact</a>
+                </div>
+                </div>
+            </div>
+        </div>
+        {/* header page contact end */}
+
+
+        {/* main header contact start*/}
         <div className="container">
             <div className="section-header text-center">
-                <h2>Contact Us</h2>
                 <p>
-                    We are always ready to assist you with your inquiries, feedback, or
-                    questions. Feel free to reach out through the contact information below.
+                    Get In Touch
                 </p>
+                <h2>Contact for any query</h2>
+
             </div>
 
             <div className="footer-row">
@@ -54,6 +72,7 @@ function Contact() {
 
                 <div className="contact-map">
                     <iframe
+                    title="Google Maps - Location of Labubu Car Wash"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1600663868074!5m2!1sen!2sbd"
                     frameBorder="0"
                     style={{
@@ -63,6 +82,10 @@ function Contact() {
                 </div>
             </div>
         </div>
+        {/* main header contact end */}
+
+
+        
     </div>
 
   );
