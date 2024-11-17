aafa774742a3f7ee27db4b7e71f814e3077a57f7
Mayjennnn <Rarentewanmayjen@gmail.com>
2024-11-17 19:35:44 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './index.css';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 import Price from "./components/Price";
+import Payment from "./components/Payment";
+import Contact from "./components/Contact";
+import Booking from "./components/Booking";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return (
-    <>
-    
-      <Price/>
-    </>
+    return (
+        <Router>
+            
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />}/>
+              <Route path="/about" element={<About />} />
+              <Route path="/price" element={<Price />} />
+              <Route path="/payment" element={<Payment />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/booking" element={<Booking />} />
+            </Routes>
+            <About />
+            
+            
+            <Price />
+            <Footer />
+        </Router>
   );
 };
 
