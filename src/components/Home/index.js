ab09045ac9417c3f330339d7fffa8c0818fb0b06
GivenPantouw07 <givenazarya@gmail.com>
2024-11-17 18:47:13 +0800

    fixed some missing codes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React from "react";
+import About from "../About";
+import Service from "../Service";
+import Facts from "../Facts"
+import Price from "../Price";
+import Footer from "../Footer";
 
 function Home() {
   return (
-    <div></div>
+    <div>
+      <About />
+      <Service />
+      <Facts />
+      <Price />
+      <Footer />
+    </div>
   );
-};
+}
 
 export default Home;
--- src/components/Price/index.js
+++ src/components/Price/index.js
@@ -1,156 +1,141 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Price = () => {
   return (
     <div>
-      {/* header page */}
-      <div className="page-header">
+      <div className="price">
         <div className="container">
+          <div className="section-header text-center">
+            <p>Washing Plan</p>
+            <h2>Choose Your Plan</h2>
+          </div>
           <div className="row">
-            <div className="col-12">
-              <h2>Washing Plan</h2>
+            <div className="col-md-4">
+              <div className="price-item">
+                <div className="price-header">
+                  <h3>Basic Cleaning</h3>
+                  <h2>
+                    <span>$</span>
+                    <strong>25</strong>
+                    <span>.99</span>
+                  </h2>
+                </div>
+                <div className="price-body">
+                  <ul>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Seats Washing
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Vacuum Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Exterior Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-times-circle" />
+                      Interior Wet Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-times-circle" />
+                      Window Wiping
+                    </li>
+                  </ul>
+                </div>
+                <div className="price-footer">
+                  <Link className="btn btn-custom" to="/booking">
+                    Book Now
+                  </Link>
+                </div>
+              </div>
             </div>
-            <div className="col-12">
-              <a href="/">Home</a>
-              <a href="/price">Price</a>
+            <div className="col-md-4">
+              <div className="price-item featured-item">
+                <div className="price-header">
+                  <h3>Premium Cleaning</h3>
+                  <h2>
+                    <span>$</span>
+                    <strong>35</strong>
+                    <span>.99</span>
+                  </h2>
+                </div>
+                <div className="price-body">
+                  <ul>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Seats Washing
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Vacuum Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Exterior Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Interior Wet Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-times-circle" />
+                      Window Wiping
+                    </li>
+                  </ul>
+                </div>
+                <div className="price-footer">
+                  <Link className="btn btn-custom" to="/booking">
+                    Book Now
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="price-item">
+                <div className="price-header">
+                  <h3>Complex Cleaning</h3>
+                  <h2>
+                    <span>$</span>
+                    <strong>49</strong>
+                    <span>.99</span>
+                  </h2>
+                </div>
+                <div className="price-body">
+                  <ul>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Seats Washing
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Vacuum Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Exterior Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Interior Wet Cleaning
+                    </li>
+                    <li>
+                      <i className="far fa-check-circle" />
+                      Window Wiping
+                    </li>
+                  </ul>
+                </div>
+                <div className="price-footer">
+                  <Link className="btn btn-custom" to="/booking">
+                    Book Now
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className="price">
-          <div className="container">
-            <div className="section-header text-center">
-              <p>Washing Plan</p>
-              <h2>Choose Your Plan</h2>
-            </div>
-            <div className="row">
-              <div className="col-md-4">
-                <div className="price-item">
-                  <div className="price-header">
-                    <h3>Basic Cleaning</h3>
-                    <h2>
-                      <span>$</span>
-                      <strong>25</strong>
-                      <span>.99</span>
-                    </h2>
-                  </div>
-                  <div className="price-body">
-                    <ul>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Seats Washing
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Vacuum Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Exterior Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-times-circle" />
-                        Interior Wet Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-times-circle" />
-                        Window Wiping
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="price-footer">
-                    <Link className="btn btn-custom" to="/booking">
-                      Book Now
-                    </Link>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="price-item featured-item">
-                  <div className="price-header">
-                    <h3>Premium Cleaning</h3>
-                    <h2>
-                      <span>$</span>
-                      <strong>35</strong>
-                      <span>.99</span>
-                    </h2>
-                  </div>
-                  <div className="price-body">
-                    <ul>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Seats Washing
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Vacuum Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Exterior Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Interior Wet Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-times-circle" />
-                        Window Wiping
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="price-footer">
-                    <Link className="btn btn-custom" to="/booking">
-                      Book Now
-                    </Link>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4">
-                <div className="price-item">
-                  <div className="price-header">
-                    <h3>Complex Cleaning</h3>
-                    <h2>
-                      <span>$</span>
-                      <strong>49</strong>
-                      <span>.99</span>
-                    </h2>
-                  </div>
-                  <div className="price-body">
-                    <ul>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Seats Washing
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Vacuum Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Exterior Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Interior Wet Cleaning
-                      </li>
-                      <li>
-                        <i className="far fa-check-circle" />
-                        Window Wiping
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="price-footer">
-                    <Link className="btn btn-custom" to="/booking">
-                      Book Now
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
       </div>
     </div>
   );
