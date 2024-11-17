995a5e31d0c1c5d4f42c6e837733c46d74ec0436
Mayjennnn <Rarentewanmayjen@gmail.com>
2024-11-17 13:31:31 +0800

    adding carwash in navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+
+const Navbar = () => {
+  const [blur, setBlur] = useState(false);
+  const location = useLocation();
+  
+  useEffect(() => {
+    function blurHandler() {
+      console.log("ScrollY:", window.scrollY);
+      if (window.scrollY > 100) {
+        setBlur(true);
+      } else{
+        setBlur(false);
+      }
+    }
+
+    window.addEventListener('scroll', blurHandler);
+
+    return () => {
+      window.removeEventListener('scroll', blurHandler);
+    };
+  }, []);
+
+  return (
+    <div className={`nav-bar ${blur ? 'nav-sticky' : ''}`}>
+      <nav className={`navbar  navbar-expand-lg navbar-dark ${blur ? 'navbar-blur' : ""}`}>
+
+        <div className="container">
+
+            <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+              <div className="navbar-nav mr-auto">
+                <Link to="/" className={`nav-item nav-link ${location.pathname === '/' ? 'active' : ''}`}>Home</Link>
+                <Link to="/price" className={`nav-item nav-link ${location.pathname === '/price' ? 'active' : ''}`}>Price</Link>
+                <Link to="/booking" className={`nav-item nav-link ${location.pathname === '/booking' ? 'active' : ''}`}>Booking</Link>
+                <Link to="/payment" className={`nav-item nav-link ${location.pathname === '/payment' ? 'active' : ''}`}>Payment</Link>
+                <Link to="/about" className={`nav-item nav-link ${location.pathname === '/about' ? 'active' : ''}`}>About</Link>
+                <Link to="/contact" className={`nav-item nav-link ${location.pathname === '/contact' ? 'active' : ''}`}>Contact</Link>
+                <Link to="/contact" className={`nav-item nav-link ${location.pathname === '/carwash' ? 'active' : ''}`}>carwash</Link>
+              </div>
+            </div>
+      </div>
+    </nav>
+  </div>
+
+  );
+};
+
+export default Navbar;
