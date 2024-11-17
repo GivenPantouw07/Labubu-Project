46a690daafc198403f133689d092674e07b054bc
elshaddai tambuwun <elshatambuwun17@gmail.com>
2024-11-17 16:55:04 +0700

    change navigation on  navbar

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Price } from './Price';
+export { default as Payment } from './Payment';
+export { default as Contact } from './Contact';
+export { default as Booking } from './Booking';
+export { default as Navbar} from './Navbar';
