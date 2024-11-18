bbd08a3ad4927eaa6c527dfef41ebb73f2564de7
valleriejoanna <valleriesalindeho27@gmail.com>
2024-11-19 01:17:35 +0800

    adding firebase in page Service

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,87 +1,93 @@
-import React from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import React, { useState, useEffect } from "react";
 
 const Service = () => {
+  const [service, setService] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const serviceRef = ref(db, "service");
+    onValue(serviceRef, (snapshot) => {
+      const data = snapshot.val();
+      setService(data);
+    });
+  }, []);
   return (
     <div className="service">
       <div className="container">
         <div className="section-header text-center">
-          <p>What We Do?</p>
-          <h2>Premium Washing Services</h2>
+          <p>{service.miniTitle}</p>
+          <h2>{service.Title}</h2>
         </div>
         <div className="row">
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-car-wash-1" />
-              <h3>Exterior Washing</h3>
+              <h3>{service.subTitle1}</h3>
               <p>
-                Comprehensive exterior cleaning to make your vehicle look brand
-                new.
+                {service.Text1}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
             <i className="flaticon-car-wash"></i>
-              <h3>Interior Washing</h3>
+              <h3>{service.subTitle2}</h3>
               <p>
-                Detailed interior cleaning for a fresh and comfortable ride.
+                {service.Text2}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-vacuum-cleaner" />
-              <h3>Vacuum Cleaning</h3>
+              <h3>{service.subTitle3}</h3>
               <p>
-                Thorough vacuuming to remove dust and dirt from all parts of the
-                vehicle.
+              {service.Text3}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-seat" />
-              <h3>Seats Washing</h3>
+              <h3>{service.subTitle4}</h3>
               <p>
-                Specialized seat washing to maintain cleanliness and comfort.
+              {service.Text4}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-car-service" />
-              <h3>Window Wiping</h3>
+              <h3>{service.subTitle5}</h3>
               <p>
-                Crystal-clear window cleaning to keep them spotless and
-                streak-free.
+              {service.Text5}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-car-service-2" />
-              <h3>Wet Cleaning</h3>
+              <h3>{service.subTitle6}</h3>
               <p>
-                Effective wet cleaning to remove stubborn stains from vehicle
-                surfaces.
+              {service.Text6}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-car-wash" />
-              <h3>Oil Changing</h3>
+              <h3>{service.subTitle7}</h3>
               <p>
-                High-quality oil change to ensure optimal engine performance.
+              {service.Text7}
               </p>
             </div>
           </div>
           <div className="col-lg-3 col-md-6">
             <div className="service-item">
               <i className="flaticon-brush-1" />
-              <h3>Brake Reparing</h3>
+              <h3>{service.subTitle8}</h3>
               <p>
-                Brake maintenance and repair for safety and top performance.
+              {service.Text8}
               </p>
             </div>
           </div>
