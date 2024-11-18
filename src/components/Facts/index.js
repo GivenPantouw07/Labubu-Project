882306f04fe38fd23c00bd379c99cfd47e68d38d
valleriejoanna <valleriesalindeho27@gmail.com>
2024-11-19 01:36:51 +0800

    adding firebase in page Facts

--- src/components/Facts/index.js
+++ src/components/Facts/index.js
@@ -1,6 +1,16 @@
-import React from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import React, { useState, useEffect } from "react";
 
 const Facts = () => {
+  const [facts, setFacts] = useState({});
+  useEffect(() => {
+    const db = getDatabase();
+    const factsRef = ref(db, "facts");
+    onValue(factsRef, (snapshot) => {
+      const data = snapshot.val();
+      setFacts(data);
+    });
+  }, []);
   return (
     <div
       className="facts"
@@ -13,8 +23,8 @@ const Facts = () => {
             <div className="facts-item">
               <i className="fa fa-map-marker-alt" aria-hidden="true" />{" "}
               <div className="facts-text">
-                <h3 data-toggle="counter-up">25</h3>
-                <p>Service Points</p>
+                <h3 data-toggle="counter-up">{facts.number1}</h3>
+                <p>{facts.text1}</p>
               </div>
             </div>
           </div>
@@ -22,8 +32,8 @@ const Facts = () => {
             <div className="facts-item">
               <i className="fa fa-user" aria-hidden="true" />
               <div className="facts-text">
-                <h3 data-toggle="counter-up">350</h3>
-                <p>Engineers &amp; Workers</p>
+                <h3 data-toggle="counter-up">{facts.number2}</h3>
+                <p>{facts.text2}</p>
               </div>
             </div>
           </div>
@@ -31,8 +41,8 @@ const Facts = () => {
             <div className="facts-item">
               <i className="fa fa-users" aria-hidden="true" />
               <div className="facts-text">
-                <h3 data-toggle="counter-up">1500</h3>
-                <p>Happy Clients</p>
+                <h3 data-toggle="counter-up">{facts.number3}</h3>
+                <p>{facts.text3}</p>
               </div>
             </div>
           </div>
@@ -40,8 +50,8 @@ const Facts = () => {
             <div className="facts-item">
               <i className="fa fa-check" aria-hidden="true" />
               <div className="facts-text">
-                <h3 data-toggle="counter-up">5000</h3>
-                <p>Projects Completed</p>
+                <h3 data-toggle="counter-up">{facts.number4}</h3>
+                <p>{facts.text4}</p>
               </div>
             </div>
           </div>
